3cabc4cd21086051ed3fb1d74b59f2a456110c2e
jh8oh <ohjiho.ca@gmail.com>
2021-08-25 03:30:48 -0400

    Removed template design

---
+++ src/views/Search.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="search">
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({})
+export default class Search extends Vue {}
+</script>
