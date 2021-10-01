fa8a22bbc0f5d0117b8054ee2b18579bd3b06ba3
jh8oh <ohjiho.ca@gmail.com>
2021-10-01 05:54:16 -0400

    Created manga model class

--- src/ts/model/manga.ts
+++ src/ts/model/manga.ts
@@ -1,1 +1,54 @@
-export default interface Manga {}
+interface Relationship {
+  id: string;
+  type: string;
+}
+
+interface Object {
+  id: string;
+  relationships: Relationship[];
+  type: string;
+}
+
+interface Attributes {
+  description: { en: string };
+  version: number;
+}
+
+interface TagAttributes extends Attributes {
+  group: string;
+  name: { en: string };
+}
+
+interface Tag extends Object {
+  attributes: TagAttributes;
+}
+
+interface Links {
+  al: string;
+  amz: string;
+  ap: string;
+  bw: string;
+  kt: string;
+  mal: string;
+  mu: string;
+}
+
+interface MangaAttributes extends Attributes {
+  altTitles: { en: string }[];
+  contentRating: string;
+  createdAt: string;
+  lastChapter: string;
+  lastVolume: string;
+  links: Links;
+  originalLanguage: string;
+  publicationDemographic: string;
+  status: string;
+  tags: Tag[];
+  title: string;
+  updatedAt: string;
+  year: string;
+}
+
+export default interface Manga extends Object {
+  attributes: MangaAttributes;
+}
