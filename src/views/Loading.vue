f35728dfa3be6b8a20da03615c988d70a766164a
jh8oh <ohjiho.ca@gmail.com>
2021-09-30 23:33:42 -0400

    Added loading page

---
+++ src/views/Loading.vue
@@ -0,0 +1,8 @@
+<template>
+  <div id="loading" class="page"></div>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+export default class Loading extends Vue {}
+</script>
