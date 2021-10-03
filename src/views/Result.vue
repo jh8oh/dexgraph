21efd72b2b0e06607828c442fb04200a4560fbaa
jh8oh <ohjiho.ca@gmail.com>
2021-10-03 00:22:42 -0400

    Added Result.vue

---
+++ src/views/Result.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="result" class="page"></div>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+
+export default class Result extends Vue {}
+</script>
