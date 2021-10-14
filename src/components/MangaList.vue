29ebd7715ce37ae19196cf0bbcfe8c31960026a7
jh8oh <ohjiho.ca@gmail.com>
2021-10-14 02:40:52 -0400

    MangaList CSS

--- src/components/MangaList.vue
+++ src/components/MangaList.vue
@@ -1,17 +1,14 @@
 <template>
-  <section>
+  <section id="manga-list">
     <h2>Manga List</h2>
     <ul>
-      <li
-        v-for="mangaFull in mangaList"
-        :key="mangaFull.manga.id"
-        @click="goToUrl(mangaFull.manga.id)"
-      >
-        <img
-          :src="`https://uploads.mangadex.org/covers/${mangaFull.manga.id}/${mangaFull.cover.attributes.fileName}.256.jpg`"
-        />
-        <h3>{{ mangaFull.manga.attributes.title.en }}</h3>
-        <p v-html="markdownToHtml(mangaFull.manga.attributes.description.en)" />
+      <li v-for="mangaFull in mangaList" :key="mangaFull.manga.id">
+        <figure @click="goToUrl(mangaFull.manga.id)">
+          <img
+            :src="`https://uploads.mangadex.org/covers/${mangaFull.manga.id}/${mangaFull.cover.attributes.fileName}.256.jpg`"
+          />
+          <figcaption>{{ mangaFull.manga.attributes.title.en }}</figcaption>
+        </figure>
       </li>
     </ul>
   </section>
@@ -21,17 +18,12 @@
 import { store } from "@/store";
 import { MangaFull } from "@/ts/model/data";
 import { Vue } from "vue-class-component";
-import marked from "marked";
 
 export default class MangaList extends Vue {
   private mangaList: MangaFull[] | null = null;
 
   mounted(): void {
     this.mangaList = store.state.followedMangas;
-  }
-
-  private markdownToHtml(markdown: string): string {
-    return marked(markdown);
   }
 
   private goToUrl(mangaId: string): void {
